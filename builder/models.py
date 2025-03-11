16d15b436d11c2c07c0a60612d3eb5270af10018
Riajul Kashem <riajul.programmer@gmail.com>
2025-03-11 23:08:28 +0600

    add website model

--- builder/models.py
+++ builder/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Website(models.Model):
+    name = models.CharField(max_length=100)
+    project_data = models.JSONField(default=dict, blank=True)  # Stores { components: html, styles: css }
+
+    def __str__(self):
+        return self.name
